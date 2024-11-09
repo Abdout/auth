e3719d7c650bdb6d6f11618db2758691fd5dd686
abdout <osmanabdout@hotmail.com>
2024-11-09 19:27:24 +0300

    x

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Any additional configuration specific to your project can be added here.
-  webpack: (config) => {
-    config.resolve.alias["@prisma/client"] = "@prisma/client/index-browser"; // Alias fix for Prisma imports in browser
-    return config;
+  future: {
+    webpack5: true, // Ensure Webpack 5 is enabled
   },
 };
 
